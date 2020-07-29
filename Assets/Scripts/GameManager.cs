ae0a459eae5fecfc83ed84a91f6b24c5558dcb57
hj-g18014 <hj-g18014@sist.ac.jp>
2020-07-29 12:45:20 +0900

    StartButtonの追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int PlayerLife;
     public GameObject PlayerPrefab;
     public Text textGameOver;
-    public int score;
-    public Text textScore;
+    private int score;
+    private float leftTime;
+    private Text textScore;
+    private Text textLife;
+    private Text textTimer;
+    private bool inGame;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,35 +23,68 @@ public class GameManager : MonoBehaviour
         PlayerLife = 3;
         textGameOver.enabled = false;
         score = 0;
+        leftTime = 30f;
         textScore = GameObject.Find("Score").GetComponent<Text>();
+        textLife = GameObject.Find("PlayerLife").GetComponent<Text>();
+        textTimer = GameObject.Find("Time").GetComponent<Text>();
         SetScoreText(score);
+        SetLifeText(PlayerLife);
+        inGame = true;
     }
+
     private void SetScoreText(int score)
     {
         textScore.text = "Score:" + score.ToString(); 
     }
 
+    private void SetLifeText(int PlayerLife)
+    {
+        textLife.text = "PlayerLife:" + PlayerLife.ToString();
+    }
+
     public void AddScoreint(int point)
     {
-        score += point;
-        SetScoreText(score);
+        if (inGame)
+        {
+            score += point;
+            SetScoreText(score);
+        }
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("stage1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GameObject PlayerObj = GameObject.Find("ty@Jumping");
-        if(PlayerObj == null)
+        if (inGame)
         {
-            --PlayerLife;
-            if(PlayerLife > 0)
+            leftTime -= Time.deltaTime;
+            textTimer.text = "Time:" + (leftTime > 0f ? leftTime.ToString("0.00") : "0.00");
+            if(leftTime < 0f)
             {
-                GameObject newPlayer = Instantiate(PlayerPrefab);
-                newPlayer.name = PlayerPrefab.name;
-            } else
-            {
-                PlayerLife = 0;
                 textGameOver.enabled = true;
+                inGame = false;
+            }
+            
+            GameObject PlayerObj = GameObject.Find("ty@Jumping");
+            if (PlayerObj == null)
+            {
+                --PlayerLife;
+                SetLifeText(PlayerLife);
+                if (PlayerLife > 0)
+                {
+                    GameObject newPlayer = Instantiate(PlayerPrefab);
+                    newPlayer.name = PlayerPrefab.name;
+                }
+                else
+                {
+                    PlayerLife = 0;
+                    textGameOver.enabled = true;
+                    inGame = false;
+                }
             }
         }
     }
