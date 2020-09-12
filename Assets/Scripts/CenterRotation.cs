63130423318658b08184d538db7c11a484d2638d
hj-g18014 <hj-g18014@sist.ac.jp>
2020-09-12 17:27:40 +0900

    ステージ制作途中

---
+++ Assets/Scripts/CenterRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterRotation : MonoBehaviour
+{
+    void Update()
+    {
+        // x軸を軸にして毎秒2度、回転させるQuaternionを作成（変数をrotとする）
+        Quaternion rot = Quaternion.Euler(0, 1, 0);
+        // 現在の自信の回転の情報を取得する。
+        Quaternion q = this.transform.rotation;
+        // 合成して、自身に設定
+        this.transform.rotation = q * rot;
+    }
+}
--- Assets/Scripts/RotationObject.cs
+++ Assets/Scripts/RotationObject.cs
@@ -6,13 +6,7 @@ public class RotationObject : MonoBehaviour
 {
     //public float rotAngle = 10.0f;
     void FixedUpdate()
-    {
-        // x軸を軸にして毎秒2度、回転させるQuaternionを作成（変数をrotとする）
-        Quaternion rot = Quaternion.AngleAxis(2, Vector3.up);
-        // 現在の自信の回転の情報を取得する。
-        Quaternion q = this.transform.rotation;
-        // 合成して、自身に設定
-        this.transform.rotation = q * rot;
-        //transform.Rotate(0f,1f, 0f);
+    { 
+        transform.Rotate(0f,1f, 0f);
     }
 }
